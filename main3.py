9f4526b51783487061442ec01f93150384590491
Ana Beatriz Kindinger <anakindinger@gmail.com>
2025-05-20 16:52:31 -0300

    feature: blur com gaussiano

--- main3.py
+++ main3.py
@@ -1,5 +1,6 @@
 #===============================================================================
 # Atividade: Implementação filtro da média
+# Estudante: Ana Beatrix Kindinger
 #-------------------------------------------------------------------------------
 # Enunciado: 
 '''Objetivo: implemente o efeito bloom em 2 versões, com filtragem Gaussiana e box blur.
@@ -23,47 +24,45 @@ import sys
 #===============================================================================
 
 INPUT_IMAGE =  'Wind Waker GC.bmp'
-LIMIAR = 125 # Limiar para o bright-pass, de 0 a 255
-SIGMAS = [1, 2, 4, 8, 16] # Valores de sigma crescentes para o filtro Gaussiano
-KERNELS = [3, 5, 7] # Tamanhos de kernel para o box blur
+LIMIAR = 115 # Limiar para o bright-pass, de 0 a 255
+SIGMAS = [4,16,32] # Valores de sigma crescentes para o filtro Gaussiano
+KERNELS = [7,9,15,19,25] # Tamanhos de kernel para o box blur
 ITERATIONS = 3 # Número de iterações para o box blur
 # CONSTANTES
 
 
 
 #===============================================================================
-#TODO
-'''
-
-2. Função de Bloom com Gaussiano:
-
-* Implementar uma função que recebe a imagem original, o limiar do bright-pass, uma lista de valores de sigma (que devem aumentar) e o tamanho do kernel Gaussiano.
-* Chamar a função de bright-pass para obter a máscara das áreas brilhantes.
-* Para cada valor de sigma na lista:
-    *Aplicar o filtro Gaussiano na imagem original usando a função do OpenCV (cv2.GaussianBlur).
-    *Aplicar a máscara das áreas brilhantes no resultado do filtro para isolar o brilho.
-* Combinar os resultados dos filtros Gaussianos com diferentes sigma (por exemplo, somando-os).
-* Adicionar a imagem combinada de volta à imagem original para criar o efeito bloom.
-* Garantir que os valores dos pixels permaneçam dentro da faixa válida (0-255).'''
 def gaussian_bloom (img, sigmas, kernel_size):
+    ''' Função de Bloom com filtro Gaussiano:
+    Entrada:
+    img é a máscara do bright-pass obtida na função bright_pass.
+    sigmas é uma lista de valores de sigma crescentes para o filtro Gaussiano.
+    kernel_size é o tamanho do kernel para o filtro Gaussiano.
+    Saída:
+    img_gbloom é a imagem resultante do efeito bloom com filtro Gaussiano para ser somada à imagem original.
+    '''
     img_gbloom = np.zeros_like (img)
-    #for s in sigmas:
+    for s in sigmas:
+        # Aplica o filtro Gaussiano na imagem
+        mais_gbloom = cv2.GaussianBlur (img, (kernel_size, kernel_size), s)
+        # Aplica a máscara das áreas brilhantes no resultado do filtro
+        img_gbloom = cv2.addWeighted (img_gbloom, 1.0, mais_gbloom, 1.0, 0)
     return img_gbloom
 
-def box_bloom(img, kernels, iterations):
-    '''3. Função de Bloom com Box Blur:
+def box_bloom(img, kernels):
+    ''' Função de Bloom com Box Blur:
     Entrada:
     img é a máscara do bright-pass obtida na função bright_pass.
     kernels é uma lista de tamanhos de kernel para o box blur.
-    iterations é o número de iterações para cada kernel
     Saída:
     img_bbloom é a imagem resultante do efeito bloom com box blur para ser somada à imagem original.
     '''
     img_bbloom = np.zeros_like (img)
     for k in kernels:
-        for _ in range(iterations):
-            # Aplica o filtro box blur na imagem
-            img_bbloom = cv2.boxFilter (img, -1, (k, k))
+        for _ in range(ITERATIONS):# Aplica o filtro box blur na imagem
+            mais_bbloom = cv2.boxFilter (img, -1, (k, k))
+        img_bbloom = cv2.addWeighted (img_bbloom, 1.0, mais_bbloom, 0.3, 0) # Adiciona a imagem resultante do box blur à imagem de bloom
             
     return img_bbloom
 #===============================================================================
@@ -74,14 +73,13 @@ def bright_pass (img, limiar):
     O filtro bright-pass é aplicado na luminância da imagem, onde os pixels com
     luminância maior que o limiar são mantidos e os demais são zerados.
     '''
-    img_mascara = np.zeros_like (img) # Cria uma imagem de saída com o mesmo tamanho da imagem de entrada
-    for i in range (img.shape[0]):
-        for j in range (img.shape[1]):
-            # Se o pixel for maior que o limiar, copia o pixel para a imagem de saída
-            if img[i, j, 1] > limiar:
-                img_mascara[i, j] = img[i, j]
-            else:
-                img_mascara[i, j] = 0
+    img_hls = cv2.cvtColor (img, cv2.COLOR_RGB2HLS)
+    img_mascara = np.zeros_like (img_hls) # Cria uma imagem de saída com o mesmo tamanho da imagem de entrada
+
+    luminancia = img_hls[:, :, 1] # Extrai o canal de luminância da imagem HLS
+    condicao = luminancia > limiar # Cria uma máscara booleana com os pixels que atendem ao critério do limiar
+
+    img_mascara = np.where(condicao[:, :, np.newaxis], img_hls, np.zeros_like(img_hls)) # Cria a máscara com os pixels que atendem ao critério do limiar
 
     return img_mascara
 
@@ -104,15 +102,23 @@ def main ():
         sys.exit ()
     cv2.imshow('Original', img)
 
+    mascara = bright_pass (img, LIMIAR)# Aplica o filtro bright-pass na imagem
+    mascaraRGB = cv2.cvtColor (mascara, cv2.COLOR_HLS2RGB) # Converte a imagem de HLS para RGB
+    cv2.imshow ('Mascara', mascaraRGB)
 
-    img = cv2.cvtColor (img, cv2.COLOR_RGB2HLS)
-    
-    # Aplica o filtro bright-pass na imagem
-    img_mascara = bright_pass (img, LIMIAR)
-    #img_mascara = cv2.cvtColor (img_mascara, cv2.COLOR_HLS2RGB)
-    cv2.imshow ('Bright-pass', img_mascara)
-
+    gbloom = gaussian_bloom (mascaraRGB, SIGMAS, KERNELS[1]) # Aplica o filtro Gaussiano na imagem
+    cv2.imshow ('Bloom Gaussiano', gbloom)
    
+    bbloom = box_bloom (mascaraRGB, KERNELS) # Aplica o filtro box blur na imagem
+    cv2.imshow ('Bloom Box Blur', bbloom)
+
+    # Combina a imagem original com os efeitos de bloom
+    img_gbloom = cv2.addWeighted (img, 1.0, gbloom, 0.3, 0)
+    img_bbloom = cv2.addWeighted (img, 1.0, bbloom, 0.3, 0)
+
+    cv2.imshow ('Bloom Gaussiano + Original', img_gbloom)
+    cv2.imshow ('Bloom Box Blur + Original', img_bbloom)
+
 
     cv2.waitKey (0)
     cv2.destroyAllWindows ()
